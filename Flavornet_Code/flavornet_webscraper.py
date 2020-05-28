c1ae8a98c75970ff60f9c8ad60e287558a23c8c8
amantrav <amantrav@umich.edu>
2020-05-28 01:01:16 -0400

    organizing into directories

---
+++ Flavornet_Code/flavornet_webscraper.py
@@ -0,0 +1,28 @@
+from bs4 import BeautifulSoup
+import csv
+
+odors_and_odorants = []
+
+soup = BeautifulSoup(open('odorants.html'), 'html.parser')
+
+odorant_tags = soup.findAll('td', {'class': 'ch'})
+odor_tags = soup.findAll('td', {'class': 'sm'})
+
+#print(len(odor_tags), len(odorant_tags))   #738 molecules, first row is empty
+
+for index in range(1, len(odor_tags)):
+    odorant = odorant_tags[index].text.strip('\n') # Get the odorant from the tag and strip newline from the name
+    odor = odor_tags[index].text # Get the odor from the tag
+
+    temp = [odorant, odor]
+    odors_and_odorants.append(temp)
+
+
+# Write to csv file
+with open('/Users/aravind/PycharmProjects/MachineOlfaction/Datasets/flavornet_dataset.csv', 'w+') as file:
+    header = ['Odorant', 'Odor']
+    writer = csv.writer(file)
+    writer.writerow(header)
+    writer.writerows(odors_and_odorants)
+
+
